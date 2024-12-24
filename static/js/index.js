2b0df93d32162457d0b2c408c5640eee8a9273e7
Forrest-110 <1558433163@qq.com>
2024-12-23 21:13:34 -0800

    gallery

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,18 @@
-window.HELP_IMPROVE_VIDEOJS = false;
-
-
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-
-})
+document.addEventListener('DOMContentLoaded', () => {
+	var options = {
+			  slidesToScroll: 3,
+			  slidesToShow: 3,
+			  loop: true,
+			  infinite: true,
+			  initialSlide: 2,
+			  autoplay: false,
+			  autoplaySpeed: 3000,
+			  pagination: false,
+	};
+  
+	// Initialize all div with carousel class
+	var carousels = bulmaCarousel.attach('.carousel', options);
+  });
+  
+  window.HELP_IMPROVE_VIDEOJS = false;
+  
